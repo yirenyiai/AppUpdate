621bc384c8656f2c06a05d047b543e064395545b
yirenyiai <yirenyiai@gmail.com>
2015-03-01 16:11:58 +0800

    增加zlib进行解压和压缩

--- AppUpdate/Main.cpp
+++ AppUpdate/Main.cpp
@@ -7,6 +7,7 @@
 #include "AppVersion.h"
 #include "AppUpdate.h"
 #include "AppFileHandle.h"
+#include "uncompress.hpp"
 
 /**
 *　　　　　　　 ┏┓　　┏┓+ +
@@ -37,9 +38,20 @@
 void OnFileDownLoadComplete(bool bResult, const boost::filesystem::path& FilePath, boost::shared_ptr<AppFileHandle> pAppFileHandle)
 {
 	printf("文件 : %s 下载完毕\n", FilePath.string().c_str());
-	if (pAppFileHandle->Copy(FilePath, boost::filesystem::current_path() / FilePath.filename()))
+	boost::filesystem::path DestPath = boost::filesystem::current_path() / FilePath.filename();
+	if (pAppFileHandle->Copy(FilePath, DestPath))
 	{
 		std::cout << FilePath.filename() << "文件处理完毕" << std::endl;
+		std::string ext = boost::filesystem::extension(FilePath);
+		if (std::string(".zip") ==  ext)
+		{
+			do_extract_zip(FilePath.string().c_str(), ".\\");
+		}
+		else if (std::string(".gz") == ext)
+		{
+			do_extract_gz(FilePath.string().c_str());
+
+		}
 	}
 }
 
